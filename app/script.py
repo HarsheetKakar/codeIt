c4e55830514caa7e688bd9340dd893c115f3dd1e
Harsheet-saxena <harshsaxena1999@gmail.com>
2019-11-18 20:38:41 +0530

    SSE client added

--- app/script.py
+++ app/script.py
@@ -15,41 +15,47 @@ db = firebase.database()
 
 users = db.child('Code').get()
 
-for user in users.each():
-    key = user.key()
-    user = user.val()
-    if(user["language"] == "Python"):
-        print("python")
-        code = user["codeBody"]
-        p = open("./python/py.py",'w')
-        p.write(code)
-        p.close()
-        os.system("python ./python/py.py > result.txt")
+def stream_handler(message):
+    user = message["data"]
+    key = message["path"]
+    print(user)
+    try:
+        if(user["language"] == "Python"):
+            print("python")
+            code = user["codeBody"]
+            p = open("./python/py.py",'w')
+            p.write(code)
+            p.close()
+            os.system("python ./python/py.py > result.txt")
 
-    elif(user["language"] == "C++"):
-        print("c++")
-        code = user["codeBody"]
-        p = open("./c/cpp.cpp","w")
-        p.write(code)
+        elif(user["language"] == "C++"):
+            print("c++")
+            code = user["codeBody"]
+            p = open("./c/cpp.cpp","w")
+            p.write(code)
+            p.close()
+            if(platform.system() == "Windows"):
+                print("c++ windows")
+                os.system("g++ -o cpp ./c/cpp.cpp&cpp.exe > result.txt")
+            elif(platform.system() == "Linux"):
+                print("c++ Linux")
+                os.system("g++ ./c/cpp.cpp")
+                print("compiled")
+                os.system("chmod +x a.out")
+                os.system("./a.out > result.txt")
+                print("result given")
+        p = open("result.txt","r")
+        ans = p.read()
         p.close()
-        if(platform.system() == "Windows"):
-            print("c++ windows")
-            os.system("g++ -o cpp ./c/cpp.cpp&cpp.exe > result.txt")
-        elif(platform.system() == "Linux"):
-            print("c++ Linux")
-            os.system("g++ ./c/cpp.cpp")
-            print("compiled")
-            os.system("chmod +x a.out")
-            os.system("./a.out > result.txt")
-            print("result given")
-    p = open("result.txt","r")
-    ans = p.read()
-    p.close()
-    result = {
-    "sender": user["sender"],
-    "result": ans,
-    "debugResult": "",
-    }
-    result = json.dumps(result)
-    db.child("Result").child(key).set(result)
-    print("result sent")
+        result = {
+        "sender": user["sender"],
+        "result": ans,
+        "debugResult": "",
+        }
+        result = json.dumps(result)
+        db.child("Result").child(key).set(result)
+        print("result sent")
+    except TypeError:
+        pass
+
+my_stream = db.child("Code").stream(stream_handler)
